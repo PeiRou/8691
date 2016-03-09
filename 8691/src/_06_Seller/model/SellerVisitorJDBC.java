3a0a40db21b889f48a5a4a617468818454c6c301
eeit83team05 <s1015408@outlook.com>
2016-03-09 19:44:43 +0800

    0309

--- 8691/src/_06_Seller/model/SellerVisitorJDBC.java
+++ 8691/src/_06_Seller/model/SellerVisitorJDBC.java
@@ -45,11 +45,11 @@ public class SellerVisitorJDBC {
 //		 System.out.println(beans);
 
 		// update OK
-//		 SellerVisitorBean beanupdate = dao.update("9999911","玖壹壹"," ","0222227911","A","200","1001","69號52樓","CCC2@hotmail.com","上原亞依","0912345678","1",false,false,10000,10000, "4DDF4A66-2618-4A89-BB32-01BB25E36E2B");
+//		 SellerVisitorBean beanupdate = dao.update("9999911","玖壹壹"," ","0222227911","A","200","1001","69號52樓","CCC2@hotmail.com","上原亞依","0912345678","1","0",10000,10000, "4DDF4A66-2618-4A89-BB32-01BB25E36E2B");
 //		 System.out.println(beanupdate);           
 
 		// insert OK
-//		SellerVisitorBean beanIns = new SellerVisitorBean(RD,"99999999","玖壹壹"," ","02227890","A","200","1001","69號52樓","CCC2@hotmail.com","上原亞依","0998987987","3",false,false,10000,10000,"2016/1/1");
+//		SellerVisitorBean beanIns = new SellerVisitorBean(RD,"99999999","玖壹壹"," ","02227890","A","200","1001","69號52樓","CCC2@hotmail.com","上原亞依","0998987987","3","0",10000,10000,"2016/1/1");
 //		dao.insert(beanIns);                              
 
 		// delete  OK
@@ -240,70 +240,68 @@ public class SellerVisitorJDBC {
 		return result;
 	}
 
-//	private static final String UPDATE = "update Seller_visitor set  FEIN=?, name=?, Seller_photo=?, tel=?, GUAR_CT=?, GUAR_AR=?, GUAR_ROAD=?, GUAR_NO=?, email2=?, Con_name=?, Con_cel=?, Seller_status=? , receipts_metho=?, IS_check=?,Ship_price=?, Lowest_price=? where Account_UID=?";
-//
-//	public SellerVisitorBean update(String FEIN, String name, String Seller_photo,String tel, String GUAR_CT, String GUAR_AR, String GUAR_ROAD, String GUAR_NO,String email2, 
-//			String Con_name, String Con_cel,String receipts_metho, boolean IS_check, boolean Seller_status, int Ship_price, int Lowest_price, String Account_UID) {
-//		Connection conn = null;
-//		PreparedStatement psStrUpd = null;
-//		SellerVisitorBean result = null;
-//		try {
-////			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-//			conn = dataSource.getConnection();
-//			psStrUpd = conn.prepareStatement(UPDATE);			
-//			psStrUpd.setString(1, FEIN);
-//			psStrUpd.setString(2, name);
-//			psStrUpd.setString  (3, Seller_photo);
-//			psStrUpd.setString(4, tel);
-//			psStrUpd.setString(5, GUAR_CT);
-//			psStrUpd.setString(6, GUAR_AR);
-//			psStrUpd.setString(7, GUAR_ROAD);
-//			psStrUpd.setString(8, GUAR_NO);
-//			psStrUpd.setString(9, email2);
-//			psStrUpd.setString(10, Con_name);
-//			psStrUpd.setString(11, Con_cel);
-//			psStrUpd.setString(12, receipts_metho);
-//			psStrUpd.setBoolean(13, IS_check);
-////			psStrUpd.setBoolean(14, IS_cooperation);	
-////			psStrUpd.setString(15, insdate);
-//			psStrUpd.setBoolean(14,Seller_status);
-//			psStrUpd.setInt(15, Ship_price);
-//			psStrUpd.setInt(16, Lowest_price);
-////			if (insdate != null) {
-////				long time = insdate.getTime();
-////				psStrUpd.setDate(15, new java.sql.Date(time));
-////			} else {
-////				psStrUpd.setDate(15, null);
-////			}			
-//			psStrUpd.setString(17, Account_UID);
-//			
-//			
-//			int i = psStrUpd.executeUpdate();
-//			if (i == 1) {
-//				System.out.println("UPDATE Success!");
-//				result = this.select(Account_UID);
-//			}
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		} finally {
-//			if (psStrUpd != null) {
-//				try {
-//					psStrUpd.close();
-//				} catch (SQLException e) {
-//					e.printStackTrace();
-//				}
-//			}
-//			if (conn != null) {
-//				try {
-//					conn.close();
-//				} catch (SQLException e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		}
-//		return result;
-//	}
-//
+	private static final String UPDATE = "update Seller_visitor set  FEIN=?, name=?, Seller_photo=?, tel=?, GUAR_CT=?, GUAR_AR=?, GUAR_ROAD=?, GUAR_NO=?, email2=?, Con_name=?, Con_cel=?, Seller_status=? , receipts_metho=?, Seller_status,Ship_price=?, Lowest_price=? where Account_UID=?";
+
+	public SellerVisitorBean update(String FEIN, String name, String Seller_photo,String tel, String GUAR_CT, String GUAR_AR, String GUAR_ROAD, String GUAR_NO,String email2, 
+			String Con_name, String Con_cel,String receipts_metho, String Seller_status, int Ship_price, int Lowest_price, String Account_UID) {
+		Connection conn = null;
+		PreparedStatement psStrUpd = null;
+		SellerVisitorBean result = null;
+		try {
+//			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			conn = dataSource.getConnection();
+			psStrUpd = conn.prepareStatement(UPDATE);			
+			psStrUpd.setString(1, FEIN);
+			psStrUpd.setString(2, name);
+			psStrUpd.setString  (3, Seller_photo);
+			psStrUpd.setString(4, tel);
+			psStrUpd.setString(5, GUAR_CT);
+			psStrUpd.setString(6, GUAR_AR);
+			psStrUpd.setString(7, GUAR_ROAD);
+			psStrUpd.setString(8, GUAR_NO);
+			psStrUpd.setString(9, email2);
+			psStrUpd.setString(10, Con_name);
+			psStrUpd.setString(11, Con_cel);
+			psStrUpd.setString(12, receipts_metho);	
+//			psStrUpd.setString(15, insdate);
+			psStrUpd.setString(13,Seller_status);
+			psStrUpd.setInt(14, Ship_price);
+			psStrUpd.setInt(15, Lowest_price);
+//			if (insdate != null) {
+//				long time = insdate.getTime();
+//				psStrUpd.setDate(15, new java.sql.Date(time));
+//			} else {
+//				psStrUpd.setDate(15, null);
+//			}			
+			psStrUpd.setString(16, Account_UID);
+			
+			
+			int i = psStrUpd.executeUpdate();
+			if (i == 1) {
+				System.out.println("UPDATE Success!");
+				result = this.select(Account_UID);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (psStrUpd != null) {
+				try {
+					psStrUpd.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return result;
+	}
+
 //	private static final String DELETE = "delete from Seller_visitor where Account_UID=?";
 //
 //	public int delete(String Account_UID) {
