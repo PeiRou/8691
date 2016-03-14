c7eae8264c8c1711fe8abc32bea300e1f2863496
eeit83team05 <s1015408@outlook.com>
2016-03-14 15:43:38 +0800

    1

--- 8691/src/_00_Account/model/PasswordServlet.java
+++ 8691/src/_00_Account/model/PasswordServlet.java
@@ -48,9 +48,9 @@ public class PasswordServlet extends HttpServlet {
     				error.put("NewPsd1", "請再次輸入新密碼");
     			}System.out.println(NewPsd1);
     			
-    			if(NewPsd!=NewPsd1){
-    				error.put("NewPsdError", "密碼不一致，請從新輸入");
-    			}
+//    			if(NewPsd!=NewPsd1){
+//    				error.put("NewPsdError", "密碼不一致，請從新輸入");
+//    			}
 
     			System.out.println("newpsd: "+NewPsd+ "   newpsd1: "+NewPsd1);
     			
